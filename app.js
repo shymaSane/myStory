f76ececd6157c3dde946128eb78dd012749d3622
shymaSane <shymaSane1@gmail.com>
2018-12-06 16:38:17 +0200

    separate routes for login&signup

--- app.js
+++ app.js
@@ -7,20 +7,20 @@ const port = 3000
 //static files:
 app.use(express.static(path.join(__dirname + '/public')));
 
+//bring routers:
+const users = require('./routes/users')
+
 //handlebars middleware:
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
-
 
 
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/users/login', (req, res) => {
-    res.render('login')
-})
+//using routes
+app.use('/users', users)
 
 app.listen(port, () =>{
     console.log(`server is working on port: ${port}`)
