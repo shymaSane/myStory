b78f2b7aab10246c247ea25e0fd006d91b83e1f1
shymaSane <shymaSane1@gmail.com>
2018-12-06 10:30:41 +0200

    company logo in navbar

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
+var exphbs  = require('express-handlebars');
+const path = require('path');
 const app = express();
 const port = 3000
 
+//static files:
+app.use(express.static(path.join(__dirname + '/public')));
+
+//handlebars middleware:
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+
+
 app.get('/', (req, res) => {
-    res.send('server is working')
+    res.render('home')
 })
 
 app.listen(port, () =>{
