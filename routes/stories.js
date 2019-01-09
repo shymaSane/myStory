4594da47c7dd2cbb195e44c348ffa69b450405f9
shymaSane <shymaSane1@gmail.com>
2019-01-09 18:27:18 +0200

    setup stories

--- routes/stories.js
+++ routes/stories.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/stories', (req, res) => {
-    res.render('./stories/story')
+    res.render('./stories/stories')
 })
 
 module.exports = router
